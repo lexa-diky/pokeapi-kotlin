5ed3d0ce317b103f26f69f677796ba1a35b67c1a
Aleksey <alexey.yakovlef@gmail.com>
2023-01-28 15:42:09 +0700

    fixed publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     kotlin("jvm") version "1.7.20"
     kotlin("plugin.serialization") version "1.7.20"
     id("org.jetbrains.dokka") version "1.7.20"
-    id("io.gitlab.arturbosch.detekt") version "1.22.0-RC1"
+    id("io.gitlab.arturbosch.detekt") version "1.22.0"
     id("maven-publish")
 }
 
@@ -40,4 +40,15 @@ detekt {
     buildUponDefaultConfig = true
     allRules = false
     config = files("$projectDir/config/detekt.yml") // point to your custom config defining rules to run, overwriting default behavior
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("pokeapi") {
+            groupId = "com.github.lexadiky.pokeapi"
+            artifactId = "core"
+            version = "1.0"
+            from(components["java"])
+        }
+    }
 }
